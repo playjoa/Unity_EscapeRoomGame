b5ed89f74276bb2d2beee4875eaaba308b4f0ccb
Joao Milone <joaocmil@hotmail.com>
2021-04-28 22:05:27 -0300

    Added Resolution Options
    Other tweaks

--- Unity Files/Assets/Scripts/Other_Scripts/ConfigScript.cs
+++ Unity Files/Assets/Scripts/Other_Scripts/ConfigScript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,9 +10,66 @@ public class ConfigScript : MonoBehaviour
     [SerializeField]
     private Color selectedColor, notSelectedColor;
 
+    [SerializeField]
+    private Dropdown dropDownResolutionOptions;
+
+    private Resolution[] availableResolutions;
+
     private void OnEnable()
     {
         ChangeButtonColor(QualitySettings.GetQualityLevel());
+        SetUpResolutionOptions();
+    }
+
+    void SetUpResolutionOptions()
+    {
+        List<string> resolutionNames = new List<string>();
+        int idCurrentResolution = 0;
+
+        availableResolutions = Screen.resolutions;
+        dropDownResolutionOptions.ClearOptions(); 
+
+        for (int i = 0; i < availableResolutions.Length; i++)
+        {
+            resolutionNames.Add(ResolutionName(availableResolutions[i]));
+
+            if (isResolutionCurrentResultion(availableResolutions[i]))
+                idCurrentResolution = i;
+
+        }
+
+        dropDownResolutionOptions.AddOptions(resolutionNames);
+
+        dropDownResolutionOptions.value = idCurrentResolution;
+        dropDownResolutionOptions.RefreshShownValue();
+    }
+
+    bool isResolutionCurrentResultion(Resolution res)
+    {
+        if (res.width != Screen.currentResolution.width)
+            return false;
+
+        if (res.height != Screen.currentResolution.height)
+            return false;
+
+        if (res.refreshRate != Application.targetFrameRate)
+            return false;
+
+        return true;
+    }
+
+    string ResolutionName(Resolution res)
+    { 
+        return res.width + " x " + res.height + " - " + res.refreshRate + " Hz"; 
+    }
+
+    public void SetResolution(int idDesiredResolution)
+    {
+        Resolution resolutionToSet = availableResolutions[idDesiredResolution];
+        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = resolutionToSet.refreshRate;
+        Screen.SetResolution(resolutionToSet.width, resolutionToSet.height, Screen.fullScreen); 
     }
 
     public void SwitchGraphics(int graphicsValue)
